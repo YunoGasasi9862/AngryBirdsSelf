091d577fc042e7fe28d64751f7ef881be355ecca
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2022-08-14 20:36:51 +0300

    ss

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -8,11 +8,10 @@ public class Enemy : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.collider.GetComponent<Fish>())
+         if(collision.collider.GetComponent<Fish>())
         {
             Destroy(gameObject);
         }
-
 
          if(collision.collider.GetComponent<Enemy>())
         {
